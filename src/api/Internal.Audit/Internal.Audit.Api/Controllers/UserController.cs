b0811b8366a5b2c6b8616cdec2c92a06093c3b93
Tanjeer Bin Haque <tbh.tilok@live.com>
2022-03-24 23:24:23 +0600

    committed with presentation layer implementation

---
+++ src/api/Internal.Audit/Internal.Audit.Api/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Internal.Audit.Application.Features.Users.Commands.AddUser;
+using Internal.Audit.Application.Features.Users.Commands.DeleteUser;
+using Internal.Audit.Application.Features.Users.Commands.UpdateUser;
+using Internal.Audit.Application.Features.Users.Queries.GetUserList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Internal.Audit.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator madiator)
+        {
+            _mediator = madiator ?? throw new ArgumentNullException(nameof(madiator));
+        }
+
+        [HttpGet("all/{activeOnly}")]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> Get(bool activeOnly)
+        {
+            var query = new GetUserListQuery(activeOnly);
+            var users = await _mediator.Send(query);
+            return Ok(users);
+
+        }
+
+        [HttpPost()]
+        public async Task<ActionResult<AddUserResponseDTO>> Add(AddUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+
+        }
+
+        [HttpPut()]
+        public async Task<ActionResult<UpdateUserResponseDTO>> Update(UpdateUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UpdateUserResponseDTO>> Delete(long id)
+        {
+            var command = new DeleteUserCommand(id);
+            var result = await _mediator.Send(command);
+            return Ok(result);
+
+        }
+    }
+}
--- src/api/Internal.Audit/Internal.Audit.Application/Features/Users/Commands/DeleteUser/DeleteUserCommand.cs
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -6,4 +6,9 @@ namespace Internal.Audit.Application.Features.Users.Commands.DeleteUser;
 public class DeleteUserCommand : IRequest<DeleteUserResponseDTO>
 {
     public long Id { get; set; }
+
+    public DeleteUserCommand(long id)
+    {
+        Id = id;
+    }
 }

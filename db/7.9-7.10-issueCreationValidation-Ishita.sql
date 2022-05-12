7a34f0aa63200b00affac5f25be0712d64cd2368
Ms.Ishita Sur Apan <Ishita.bd@asa-international.com>
2022-05-12 14:23:58 +0600

    Initial commit for 7.9, 7.10

---
+++ db/7.9-7.10-issueCreationValidation-Ishita.sql
@@ -0,0 +1,112 @@
+--7.9
+CREATE TABLE [seven].[Issue](
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
+	[IssueId] [nvarchar](20) NOT NULL UNIQUE,
+	[IssueTitle] [nvarchar](MAX) NOT NULL,
+	[AuditId] [uniqueidentifier] NOT NULL, --FK of Audit
+	[AuditScheduleId] [uniqueidentifier] NOT NULL, --FK for Schedule
+	[BranchId] [uniqueidentifier] NOT NULL, --FK for Branch
+	[IssueOwnerId] [BIGINT] NOT NULL FOREIGN KEY REFERENCES [security].[User](Id),
+	[Policy] [nvarchar](MAX) NOT NULL,
+	[ImpactTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [Config].[IssueImpactType](Id),
+	[LikelihoodTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[IssueLikelihoodType](Id),
+	[RatingTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[RatingType](Id),
+	[TargetDate] [datetime] NOT NULL,
+	[Details] [nvarchar](MAX) NOT NULL,
+	[RootCause] [nvarchar](MAX) NOT NULL,
+	[BusinessImpact] [nvarchar](MAX) NOT NULL,
+	[PotentialRisk] [nvarchar](MAX) NOT NULL,
+	[AuditorRecommendation] [nvarchar](MAX) NOT NULL,
+	[Status] [uniqueidentifier] NULL FOREIGN KEY REFERENCES [config].[IssueStatusType](Id),
+	[Remarks] [nvarchar](MAX) NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0,
+	)
+
+
+CREATE TABLE [config].[IssueImpactType](
+	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0
+	)
+
+CREATE TABLE [config].[IssueLikelihoodType](
+	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0
+	)
+
+CREATE TABLE [config].[IssueStatusType](
+	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+	[CreatedBy] [nvarchar](10) NULL,
+	[CreatedOn] [datetime] NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0
+	)
+
+CREATE TABLE [config].[RatingType](
+	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+	[CreatedBy] [nvarchar](10) NULL,
+	[CreatedOn] [datetime] NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0
+	)
+--7.10
+CREATE TABLE [seven].[IssueValidation](
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
+	[IssueId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [seven].[Issue](Id),
+	[ClosureSummary] [nvarchar](max) NOT NULL,
+	[ValidatedByUserId] [bigint] NOT NULL FOREIGN KEY REFERENCES [security].[User](Id),
+	[ValidationDate] [datetime] NOT NULL,
+	[ReviewedByUserID] [bigint] NULL FOREIGN KEY REFERENCES [security].[User](Id),
+	[ReviewDate] [datetime] NULL,
+	[ReviewEvidenceFilePath] [nvarchar](MAX) NULL,
+	[ApprovedByUserId] [bigint] NULL FOREIGN KEY REFERENCES [security].[User](Id),
+	[ApprovalDate] [datetime] NULL,
+	[ApprovalEvidenceFilePath] [nvarchar](MAX) NULL,
+	[IssueClosureDate] [datetime] NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0,
+	)

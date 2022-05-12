dba566c36a8f07666619863ce3cdb231349af36e
Tanjeer <tbh.tilok@live.com>
2022-05-12 17:16:11 +0600

    committed

--- db/7.9-7.10-issueCreationValidation-Ishita.sql
+++ db/7.9-7.10-issueCreationValidation-Ishita.sql
@@ -1,24 +1,24 @@
 --7.9
 CREATE TABLE [seven].[Issue](
 	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
-	[IssueId] [nvarchar](20) NOT NULL UNIQUE,
-	[IssueTitle] [nvarchar](MAX) NOT NULL,
+	[IssueCode] [nvarchar](20) NOT NULL UNIQUE,
+	[IssueTitle] [nvarchar](100) NOT NULL,
 	[AuditId] [uniqueidentifier] NOT NULL, --FK of Audit
 	[AuditScheduleId] [uniqueidentifier] NOT NULL, --FK for Schedule
 	[BranchId] [uniqueidentifier] NOT NULL, --FK for Branch
-	[IssueOwnerId] [BIGINT] NOT NULL FOREIGN KEY REFERENCES [security].[User](Id),
-	[Policy] [nvarchar](MAX) NOT NULL,
+	[IssueOwnerId] [BIGINT] NOT NULL FOREIGN KEY REFERENCES [security].[User](Id),--todo
+	[Policy] [nvarchar](500) NOT NULL,
 	[ImpactTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [Config].[IssueImpactType](Id),
 	[LikelihoodTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[IssueLikelihoodType](Id),
 	[RatingTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[RatingType](Id),
 	[TargetDate] [datetime] NOT NULL,
-	[Details] [nvarchar](MAX) NOT NULL,
-	[RootCause] [nvarchar](MAX) NOT NULL,
-	[BusinessImpact] [nvarchar](MAX) NOT NULL,
-	[PotentialRisk] [nvarchar](MAX) NOT NULL,
-	[AuditorRecommendation] [nvarchar](MAX) NOT NULL,
+	[Details] [nvarchar](500) NOT NULL,
+	[RootCause] [nvarchar](500) NOT NULL,
+	[BusinessImpact] [nvarchar](500) NOT NULL,
+	[PotentialRisk] [nvarchar](500) NOT NULL,
+	[AuditorRecommendation] [nvarchar](500) NOT NULL,
 	[Status] [uniqueidentifier] NULL FOREIGN KEY REFERENCES [config].[IssueStatusType](Id),
-	[Remarks] [nvarchar](MAX) NULL,
+	[Remarks] [nvarchar](500) NULL, ---- status hisotry table
 	[CreatedBy] [nvarchar](10) NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
 	[UpdatedBy] [nvarchar](10) NULL,
@@ -32,15 +32,15 @@ CREATE TABLE [seven].[Issue](
 
 CREATE TABLE [seven].[Action](
 	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,	
-	[PlanId] [nvarchar](20) NOT NULL UNIQUE,
+	[PlanCode] [nvarchar](20) NOT NULL UNIQUE,
 	[IssueId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [seven].[Issue](Id),	
 	[OwnerUserId] [BIGINT] NOT NULL FOREIGN KEY REFERENCES [security].[User](Id),
-	[ManagementPlan] [nvarchar](max) NOT NULL,
+	[ManagementPlan] [nvarchar](500) NOT NULL,
 	[TargetDate] [datetime] NOT NULL,
 	[IsActionTaken] [bit] NOT NULL DEFAULT 0,
 	[ActionTakenDate] [datetime] NULL,
-	[EvidenceFilePath] [nvarchar](MAX) NULL,
-	[ActionTakenRemarks] [nvarchar](MAX) NULL,
+	[EvidenceFilePath] [nvarchar](MAX) NULL,-- ref with document table 
+	[ActionTakenRemarks] [nvarchar](500) NULL,
 	[CreatedBy] [nvarchar](10) NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
 	[UpdatedBy] [nvarchar](10) NULL,
@@ -55,6 +55,7 @@ CREATE TABLE [seven].[Action](
 CREATE TABLE [config].[IssueImpactType](
 	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
 	[Name] [nvarchar](50) NOT NULL,
+	--IsActive
 	[CreatedBy] [nvarchar](10) NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
 	[UpdatedBy] [nvarchar](10) NULL,
@@ -69,6 +70,7 @@ CREATE TABLE [config].[IssueImpactType](
 CREATE TABLE [config].[IssueLikelihoodType](
 	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
 	[Name] [nvarchar](50) NOT NULL,
+	--IsActive
 	[CreatedBy] [nvarchar](10) NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
 	[UpdatedBy] [nvarchar](10) NULL,
@@ -83,6 +85,7 @@ CREATE TABLE [config].[IssueLikelihoodType](
 CREATE TABLE [config].[IssueStatusType](
 	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
 	[Name] [nvarchar](50) NOT NULL,
+	--IsActive
 	[CreatedBy] [nvarchar](10) NULL,
 	[CreatedOn] [datetime] NULL,
 	[UpdatedBy] [nvarchar](10) NULL,
@@ -97,6 +100,7 @@ CREATE TABLE [config].[IssueStatusType](
 CREATE TABLE [config].[RatingType](
 	[Id] [uniqueidentifier] DEFAULT NEWID() NOT NULL PRIMARY KEY,
 	[Name] [nvarchar](50) NOT NULL,
+	--IsActive
 	[CreatedBy] [nvarchar](10) NULL,
 	[CreatedOn] [datetime] NULL,
 	[UpdatedBy] [nvarchar](10) NULL,
@@ -116,10 +120,10 @@ CREATE TABLE [seven].[IssueValidation](
 	[ValidationDate] [datetime] NOT NULL,
 	[ReviewedByUserID] [bigint] NULL FOREIGN KEY REFERENCES [security].[User](Id),
 	[ReviewDate] [datetime] NULL,
-	[ReviewEvidenceFilePath] [nvarchar](MAX) NULL,
-	[ApprovedByUserId] [bigint] NULL FOREIGN KEY REFERENCES [security].[User](Id),
+	[ReviewEvidenceFilePath] [nvarchar](MAX) NULL,-- ref with document table 
+	[ApprovedByUserId] [bigint] NULL FOREIGN KEY REFERENCES [security].[User](Id),--[uniqueidentifier]
 	[ApprovalDate] [datetime] NULL,
-	[ApprovalEvidenceFilePath] [nvarchar](MAX) NULL,
+	[ApprovalEvidenceFilePath] [nvarchar](MAX) NULL,-- ref with document table 
 	[IssueClosureDate] [datetime] NULL,
 	[CreatedBy] [nvarchar](10) NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
@@ -131,3 +135,5 @@ CREATE TABLE [seven].[IssueValidation](
 	[ApprovedOn] [datetime] NULL,
 	[IsDeleted] [bit] NOT NULL DEFAULT 0,
 	)
+
+	-- IssueValidationAction table
--- db/QueryIA(EpicEight_Emu).sql
+++ db/QueryIA(EpicEight_Emu).sql
@@ -19,43 +19,41 @@ CREATE TABLE [eight].[AuditFrequency](
 )
 
 CREATE TABLE [eight].[RiskRating](
-[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
-[CountryId] [bigint] NOT NULL CONSTRAINT FK_RiskRatingMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
-[RiskRatingTypeId] [uniqueidentifier] NOT NULL CONSTRAINT FK_RiskRatingType FOREIGN KEY(RiskRatingTypeId) REFERENCES [common].[RiskRatingType](Id), 
-[MinWeightSum] [int]  NOT NULL,
-[MaxWeightSum] [int]  NOT NULL,
-[StartingDate] [datetime] NOT NULL,
-[EndingDate] [datetime] NOT NULL,
-[CreatedBy] [nvarchar](10) NOT NULL,
-[UpdatedBy] [nvarchar](10) NULL,
-[CreatedOn] [datetime] NULL,
-[UpdatedOn] [datetime] NULL,
-[IsDelete] [bit] NULL DEFAULT ((0)) ,
-[ApprovedBy] [nvarchar](10) NULL,
-[ApprovedOn] [datetime] NULL,
-[ReviewedBy] [nvarchar](10) NULL,
-[ReviewedOn] [datetime] NULL
-
-
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
+	[CountryId] [bigint] NOT NULL CONSTRAINT FK_RiskRatingMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
+	[RiskRatingTypeId] [uniqueidentifier] NOT NULL CONSTRAINT FK_RiskRatingType FOREIGN KEY(RiskRatingTypeId) REFERENCES [common].[RiskRatingType](Id), 
+	[MinWeightSum] [int]  NOT NULL,
+	[MaxWeightSum] [int]  NOT NULL,
+	[StartingDate] [datetime] NOT NULL,
+	[EndingDate] [datetime] NOT NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[CreatedOn] [datetime] NULL,
+	[UpdatedOn] [datetime] NULL,
+	[IsDelete] [bit] NULL DEFAULT ((0)) ,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL
 )
 
 CREATE TABLE [eight].[RiskCriteria](
-[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
-[CountryId] [bigint] NOT NULL CONSTRAINT FK_RiskCriteriaMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
-[Name] [nvarchar](max) NOT NULL,
-[Weight] [int] NOT NULL,
-[StartingDate] [datetime] NOT NULL,
-[EndingDate] [datetime] NOT NULL,
-[Description] [nvarchar](max) NULL,
-[CreatedBy] [nvarchar](10) NOT NULL,
-[UpdatedBy] [nvarchar](10) NULL,
-[CreatedOn] [datetime] NULL,
-[UpdatedOn] [datetime] NULL,
-[IsDelete] [bit] NULL DEFAULT ((0)) ,
-[ApprovedBy] [nvarchar](10) NULL,
-[ApprovedOn] [datetime] NULL,
-[ReviewedBy] [nvarchar](10) NULL,
-[ReviewedOn] [datetime] NULL
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
+	[CountryId] [bigint] NOT NULL CONSTRAINT FK_RiskCriteriaMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
+	[Name] [nvarchar](max) NOT NULL,
+	[Weight] [int] NOT NULL,
+	[StartingDate] [datetime] NOT NULL,
+	[EndingDate] [datetime] NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[CreatedOn] [datetime] NULL,
+	[UpdatedOn] [datetime] NULL,
+	[IsDelete] [bit] NULL DEFAULT ((0)) ,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL
 
 
 )

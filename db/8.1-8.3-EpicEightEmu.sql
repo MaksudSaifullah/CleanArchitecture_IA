b183282d3025775e3241817594844b04f8f8b993
Ms. Ishrat Jahan Emu <emu.bd@asa-international.com>
2022-05-18 09:28:20 +0600

    epic eight modified

---
+++ db/8.1-8.3-EpicEightEmu.sql
@@ -0,0 +1,61 @@
+--Already Exists -- Created By Shezan bhai
+--CREATE TABLE [eight].[AuditFrequency](
+--[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
+--[CountryId] [bigint] NOT NULL CONSTRAINT FK_AuditFrequencyMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
+--[RiskRatingTypeId] [uniqueidentifier] NOT NULL CONSTRAINT FK_AuditFrequencyRiskType FOREIGN KEY(RiskRatingTypeId) REFERENCES [common].[RiskRatingType](Id), 
+--[AuditFrequencyTypeId] [uniqueidentifier] NOT NULL CONSTRAINT FK_AuditFrequencyAuditFrequency FOREIGN KEY(AuditFrequencyTypeId) REFERENCES [common].[AuditFrequencyType](Id),
+--[StartingDate] [datetime] NOT NULL,
+--[EndingDate] [datetime] NOT NULL,
+--[CreatedBy] [nvarchar](10) NOT NULL,
+--[UpdatedBy] [nvarchar](10) NULL,
+--[CreatedOn] [datetime] NULL,
+--[UpdatedOn] [datetime] NULL,
+--[IsDelete] [bit] NULL DEFAULT ((0)) ,
+--[ApprovedBy] [nvarchar](10) NULL,
+--[ApprovedOn] [datetime] NULL,
+--[ReviewedBy] [nvarchar](10) NULL,
+--[ReviewedOn] [datetime] NULL
+
+
+--)
+
+CREATE TABLE [eight].[RiskRating](
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
+	[CountryId] [bigint] NOT NULL CONSTRAINT FK_RiskRatingMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
+	[RiskRatingTypeId] [uniqueidentifier] NOT NULL CONSTRAINT FK_RiskRatingType FOREIGN KEY(RiskRatingTypeId) REFERENCES [common].[RiskRatingType](Id), 
+	[MinWeightSum] [int]  NOT NULL,
+	[MaxWeightSum] [int]  NOT NULL,
+	[StartingDate] [datetime] NOT NULL,
+	[EndingDate] [datetime] NOT NULL,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0
+)
+
+--Already Exists -- Created By Anik bhai
+--CREATE TABLE [eight].[RiskCriteria](
+--	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT (NEWID()),
+--	[CountryId] [bigint] NOT NULL CONSTRAINT FK_RiskCriteriaMap FOREIGN KEY (CountryId) REFERENCES [security].[Country](Id),  -- change with uniqueidentifier
+--	[Name] [nvarchar](max) NOT NULL,
+--	[Weight] [int] NOT NULL,
+--	[StartingDate] [datetime] NOT NULL,
+--	[EndingDate] [datetime] NOT NULL,
+--	[Description] [nvarchar](max) NULL,
+--	[CreatedBy] [nvarchar](10) NOT NULL,
+--	[UpdatedBy] [nvarchar](10) NULL,
+--	[CreatedOn] [datetime] NULL,
+--	[UpdatedOn] [datetime] NULL,
+--	[IsDelete] [bit] NULL DEFAULT ((0)) ,
+--	[ApprovedBy] [nvarchar](10) NULL,
+--	[ApprovedOn] [datetime] NULL,
+--	[ReviewedBy] [nvarchar](10) NULL,
+--	[ReviewedOn] [datetime] NULL
+
+
+--)

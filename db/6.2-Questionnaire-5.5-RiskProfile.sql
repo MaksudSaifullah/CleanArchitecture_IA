a243ec46efe48f51820ff546ce9670f66da3c07c
Ms.Ishita Sur Apan <Ishita.bd@asa-international.com>
2022-05-19 13:16:32 +0600

    5.5, 6.2

---
+++ db/6.2-Questionnaire-5.5-RiskProfile.sql
@@ -0,0 +1,38 @@
+CREATE TABLE [six].[Questionnaire](
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+	[CountryId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [common].[Country](Id),
+	[TopicHeadId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [six].[TopicHead](Id),
+	[Question] [nvarchar](500) NOT NULL,
+	[EffectiveFrom] [datetime] NOT NULL,
+	[EffectiveTo] [datetime] NOT NULL,
+	[IsActive] [bit] NOT NULL DEFAULT 0,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0,
+	)
+
+CREATE TABLE [five].[RiskProfile](
+	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT NEWSEQUENTIALID(),
+	[LikelihoodLevelTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[LikelihoodType], --will there be any generic table?
+	[ImpactLevelTypeId] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[ImpactType], --will there be any generic table?
+	[RiskRating] [uniqueidentifier] NOT NULL FOREIGN KEY REFERENCES [config].[RatingType](Id),	
+	[EffectiveFrom] [datetime] NOT NULL,
+	[EffectiveTo] [datetime] NOT NULL,
+	[Description] [nvarchar](200) NULL,
+	[IsActive] [bit] NOT NULL DEFAULT 0,
+	[CreatedBy] [nvarchar](10) NOT NULL,
+	[CreatedOn] [datetime] NOT NULL,
+	[UpdatedBy] [nvarchar](10) NULL,
+	[UpdatedOn] [datetime] NULL,
+	[ReviewedBy] [nvarchar](10) NULL,
+	[ReviewedOn] [datetime] NULL,
+	[ApprovedBy] [nvarchar](10) NULL,
+	[ApprovedOn] [datetime] NULL,
+	[IsDeleted] [bit] NOT NULL DEFAULT 0,
+	)

5c7bc154bea59f18dadf39b36d16a6d73d5c03eb
shariful <shariful.bd@asa-international.com>
2022-05-30 16:37:17 +0600

    schema wise folder pushed

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/common/Country.cs
@@ -0,0 +1,27 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.common
+{
+    [Table("Country", Schema = "common")]
+    public class Country : EntityBase
+    {
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(10)]
+        public string Code { get; set; } = null!;
+
+        //Navigation properties
+        public virtual ICollection<UserCountry> UserCountries { get; set; } = null!;
+
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/security/UserCountry.cs
@@ -0,0 +1,34 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.security
+{
+    [Table("UserCountry", Schema = "security")]
+    public class UserCountry : EntityBase
+    {
+        [Required]
+        public Guid CountryId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [DefaultValue("1")]
+        public bool IsActive { get; set; }
+
+        //Navigation properties
+        [ForeignKey("CountryId")]
+        public virtual Country Country { get; set; } = null!;
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; } = null!;
+
+    }
+}

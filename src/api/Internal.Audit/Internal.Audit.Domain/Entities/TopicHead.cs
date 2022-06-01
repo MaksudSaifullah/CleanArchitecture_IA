db2c8e40428f59c99f1cc142344307a92b9e0890
Anik Saha <82827610+aniksaha007@users.noreply.github.com>
2022-06-01 12:20:18 +0600

    IA_7_Anik

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/TopicHead.cs
@@ -0,0 +1,38 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities;
+
+[Table("TopicHead", Schema = "")]
+public class TopicHead : EntityBase
+{
+    [Required]
+    public Guid CountryId { get; set; }
+
+    [Required]
+    [DefaultValue("1")]
+    public bool IsActive { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [Required]
+    public DateTime EffectiveFrom { get; set; }
+    [Required]
+    public DateTime EffectiveTo { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Description { get; set; }
+
+    [ForeignKey("CountryId")]
+    public virtual Country Country { get; set; } = null!;
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/WeightScore.cs
@@ -0,0 +1,39 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities;
+
+
+[Table("WeightScore", Schema = "")]
+public class WeightScore : EntityBase
+{
+    [Required]
+    public Guid CountryId { get; set; }
+
+    [Required]
+    public Guid TopicHeadId { get; set; }
+
+    [Required]
+    public decimal Score { get; set; }
+
+    [Required]
+    public DateTime EffectiveFrom { get; set; }
+    [Required]
+    public DateTime EffectiveTo { get; set; }
+
+    [Required]
+    [MaxLength(300)]
+    public string Description { get; set; }
+
+    [ForeignKey("CountryId")]
+    public virtual Country Country { get; set; } = null!;
+
+    [ForeignKey("TopicHeadId")]
+    public virtual TopicHead TopicHead { get; set; } = null!;
+}

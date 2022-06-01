e9145b2cee18e5e6b0d2106e150af46f88611106
Ms.Ishita Sur Apan <Ishita.bd@asa-international.com>
2022-06-01 14:45:44 +0600

    Entities of Iftekhar vai

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/SubProcess.cs
@@ -0,0 +1,25 @@
+﻿using Internal.Audit.Domain.Common;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Internal.Audit.Domain.Entities;
+
+[Table("SubProcess", Schema = "")]
+public class SubProcess : EntityBase
+{
+    [Required]
+    public Guid ProcessId { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = null!;
+    [Required]
+    public DateTime EffeciveFrom { get; set; }
+    [Required]
+    public DateTime EffeciveTo { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
+    //[ForeignKey("ProcessId")]
+    //public virtual Process Process { get; set; } = null!;
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/UploadDocumentNotifyList.cs
@@ -0,0 +1,25 @@
+﻿using Internal.Audit.Domain.Common;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Internal.Audit.Domain.Entities;
+
+[Table("UploadDocumentNotifyList", Schema = "")]
+public class UploadDocumentNotifyList : EntityBase
+{
+    [Required]
+    public Guid DocumentId { get; set; }
+    [Required]
+    public Guid RoleId { get; set; }
+    [Required]
+    [DefaultValue(0)]
+    public bool IsEmailSent { get; set; }
+
+    //TODO
+    //[ForeignKey("DocumentId")]
+    //public virtual Document Document { get; set; } = null!;
+    //[ForeignKey("RoleId")]
+    //public virtual Role Role { get; set; } = null!;
+
+}

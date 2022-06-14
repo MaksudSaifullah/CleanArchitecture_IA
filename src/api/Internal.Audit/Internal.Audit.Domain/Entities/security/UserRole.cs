079901ae781cf3c8ee8c9f19489f83b3057d214b
iftekharalibd <ali.bd@asa-international.com>
2022-06-14 16:49:52 +0600

    User role, employee table added

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/security/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.security
+{
+    [Table("UserRole", Schema = "Security")]
+    public class UserRole:EntityBase
+    {
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/Migrations/20220614104416_Usertable Designation removed.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internal.Audit.Infrastructure.Persistent.Migrations
+{
+    public partial class UsertableDesignationremoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Designation_DesignationId",
+                schema: "Security",
+                table: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_DesignationId",
+                schema: "Security",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "DesignationId",
+                schema: "Security",
+                table: "User");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DesignationId",
+                schema: "Security",
+                table: "User",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_DesignationId",
+                schema: "Security",
+                table: "User",
+                column: "DesignationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Designation_DesignationId",
+                schema: "Security",
+                table: "User",
+                column: "DesignationId",
+                principalSchema: "Common",
+                principalTable: "Designation",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

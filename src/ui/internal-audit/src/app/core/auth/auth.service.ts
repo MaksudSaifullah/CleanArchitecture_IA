548705b0a80234f7be5c945e26b5375cf810eaff
emran <emran.bd@asa-international.com>
2022-06-15 17:22:59 +0600

    http client completed

--- src/ui/internal-audit/src/app/core/auth/auth.service.ts
+++ src/ui/internal-audit/src/app/core/auth/auth.service.ts
@@ -1,9 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpService } from '../services/http.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpService) { }
+
+  authenticate(username:string,password:string):Observable<any>{
+    return this.http.post('api/v1/authentication',{email:username,password:password});
+  }
 }

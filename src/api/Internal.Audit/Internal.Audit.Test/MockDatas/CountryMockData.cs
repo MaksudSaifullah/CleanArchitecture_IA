fd54e59e9696263cd47455fe5c262ba01ca31e4f
Anik Saha <82827610+aniksaha007@users.noreply.github.com>
2022-06-19 17:52:51 +0600

    IA-93

--- src/api/Internal.Audit/Internal.Audit.Test/MockDatas/CountryMockData.cs
+++ src/api/Internal.Audit/Internal.Audit.Test/MockDatas/CountryMockData.cs
@@ -12,7 +12,6 @@ namespace Internal.Audit.Test.MockDatas
         public static List<Country> countryList = new List<Country>();
 
         public static List<Country> updatecountryList = new List<Country>();
-        public static List<Country> deletecountryList = new List<Country>();
 
         public static List<Country> GetCountriesData()
         {
@@ -22,10 +21,7 @@ namespace Internal.Audit.Test.MockDatas
         {
             return updatecountryList;
         }
-        public static List<Country> deleteCountriesData()
-        {
-            return deletecountryList;
-        }
+ 
 
         public static void AddCountryStatic()
         {
@@ -87,60 +83,6 @@ namespace Internal.Audit.Test.MockDatas
                 });
             }
            
-        }
-        public static void deleteAddCountryStatic()
-        {
-            deletecountryList.Add(new Country
-            {
-                Id = new Guid("791D35FF-9EA2-4C7B-AA3A-840F50DC59C4"),
-                CreatedBy = "Admin",
-                CreatedOn = DateTime.Now,
-                UpdatedBy = "",
-                UpdatedOn = null,
-                ReviewedBy = "",
-                ReviewedOn = null,
-                ApprovedBy = "",
-                ApprovedOn = null,
-                IsDeleted = false,
-
-                Name = "Bangladesh",
-                Code = "BD",
-                Remarks = ""
-            });
-            deletecountryList.Add(new Country
-            {
-                Id = new Guid("496BE48B-E21A-47D5-AB85-D1A0D4EDC744"),
-                CreatedBy = "Admin",
-                CreatedOn = DateTime.Now,
-                UpdatedBy = "",
-                UpdatedOn = null,
-                ReviewedBy = "",
-                ReviewedOn = null,
-                ApprovedBy = "",
-                ApprovedOn = null,
-                IsDeleted = false,
-
-                Name = "Nepal",
-                Code = "NP",
-                Remarks = ""
-            });
-            deletecountryList.Add(new Country
-            {
-                Id = new Guid("73C94EF7-4B6D-4D86-BAFA-67252CCC2A8A"),
-                CreatedBy = "Admin",
-                CreatedOn = DateTime.Now,
-                UpdatedBy = "",
-                UpdatedOn = null,
-                ReviewedBy = "",
-                ReviewedOn = null,
-                ApprovedBy = "",
-                ApprovedOn = null,
-                IsDeleted = false,
-
-                Name = "Bhutan",
-                Code = "BT",
-                Remarks = ""
-            });
         }
         public static void updateAddCountryStatic()
         {

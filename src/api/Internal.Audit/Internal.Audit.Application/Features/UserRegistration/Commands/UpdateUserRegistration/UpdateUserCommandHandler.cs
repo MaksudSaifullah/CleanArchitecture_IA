cca7b2305e15268e4cbdc137b7e1c617758abddc
iftekharalibd <ali.bd@asa-international.com>
2022-06-21 11:01:55 +0600

    IA-83

---
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/UserRegistration/Commands/UpdateUserRegistration/UpdateUserCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿
+using AutoMapper;
+using Internal.Audit.Application.Contracts.Persistent;
+using Internal.Audit.Application.Features.Users.Commands.AddUser;
+using Internal.Audit.Domain.Entities;
+using Internal.Audit.Domain.Entities.Security;
+using MediatR;
+
+namespace Internal.Audit.Application.Features.Users.Commands.UpdateUser;
+
+public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UpdateUserResponseDTO>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserCommandRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UpdateUserCommandHandler(IUserCommandRepository userRepository, IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+
+    public async Task<UpdateUserResponseDTO> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = _mapper.Map<User>(request);
+        var modifiedUser = await _userRepository.Update(user);
+        var rowsAffected = await _unitOfWork.CommitAsync();
+        return new UpdateUserResponseDTO(modifiedUser.Id, rowsAffected > 0, rowsAffected > 0 ? "User Updated Successfully!" : "Error while updating user!");
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/UserRegistration/Commands/UpdateUserRegistration/UpdateUserCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿
+using FluentValidation;
+
+namespace Internal.Audit.Application.Features.Users.Commands.UpdateUser;
+
+public class UpdateUserCommandValidator: AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+
+        RuleFor(u => u.Email)
+            .NotEmpty().WithMessage("Email cannot be empty")
+            .MaximumLength(50).WithMessage("Email address length cannot be more than 50 characters");
+
+        RuleFor(u => u.Password)
+            .NotEmpty().WithMessage("Password cannot be empty")
+            .MaximumLength(15).WithMessage("Password length cannot be more than 15 characters")
+            .MinimumLength(8).WithMessage("Password length cannot be less than 8 characters");
+    }
+}

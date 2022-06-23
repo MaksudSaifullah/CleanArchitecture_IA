d6323b86ab67832fc0800f9d0cdcea540e4a8bb1
emran <emran.bd@asa-international.com>
2022-06-23 12:04:25 +0600

    Items Moved to Services

---
+++ src/ui/internal-audit/src/app/core/services/datatable.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+
+@Injectable({providedIn: 'root'})
+export class DatatableService {
+  redraw(datatable : DataTableDirective){
+
+  }
+}

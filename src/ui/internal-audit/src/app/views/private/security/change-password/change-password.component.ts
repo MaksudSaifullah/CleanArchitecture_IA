c5705e64d7833f2ef6a5c3ff7a2adb75cc4dd1d5
emran <emran.bd@asa-international.com>
2022-06-30 17:43:04 +0600

    change password ui & validation completed

--- src/ui/internal-audit/src/app/views/private/security/change-password/change-password.component.ts
+++ src/ui/internal-audit/src/app/views/private/security/change-password/change-password.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, MaxLengthValidator, Validators } from '@angular/forms';
+import { CutomvalidatorService } from 'src/app/core/services/cutomvalidator.service';
+import { FormService } from 'src/app/core/services/form.service';
 
 @Component({
   selector: 'app-change-password',
@@ -8,15 +10,20 @@ import { FormBuilder, FormGroup, MaxLengthValidator, Validators } from '@angular
 })
 export class ChangePasswordComponent implements OnInit {
   changePasswordForm : FormGroup;
-  constructor(private fb: FormBuilder) {
-      this.changePasswordForm = fb.group({
-        'currentPassword': ['',Validators.minLength(5)],
-        'newPassword': ['',Validators.minLength(5)],
-        'confirmPassword': ['',Validators.minLength(5)],
+  public formService: FormService;
+  constructor(private fb: FormBuilder, private cutomvalidatorService: CutomvalidatorService,private _formService : FormService) {
+    this.formService = _formService;
+    this.changePasswordForm = fb.group({
+        currentPassword: ['',[Validators.required,Validators.minLength(5)]],
+        newPassword: ['',[Validators.required,Validators.minLength(5)]],
+        confirmPassword: ['',[Validators.required,Validators.minLength(5)]],
       })
    }
 
   ngOnInit(): void {
   }
-
+  submit(){
+    if(this.changePasswordForm.valid){
+    }
+  }
 }

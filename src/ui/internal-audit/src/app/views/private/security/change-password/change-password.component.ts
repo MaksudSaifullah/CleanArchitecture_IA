95091409b1aa0025a188aaf8ade380f4943cf2c1
emran <emran.bd@asa-international.com>
2022-06-30 14:56:32 +0600

    Header Logout Fixed

--- src/ui/internal-audit/src/app/views/private/security/change-password/change-password.component.ts
+++ src/ui/internal-audit/src/app/views/private/security/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, MaxLengthValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
-
-  constructor() { }
+  changePasswordForm : FormGroup;
+  constructor(private fb: FormBuilder) {
+      this.changePasswordForm = fb.group({
+        'currentPassword': ['',Validators.minLength(5)],
+        'newPassword': ['',Validators.minLength(5)],
+        'confirmPassword': ['',Validators.minLength(5)],
+      })
+   }
 
   ngOnInit(): void {
   }

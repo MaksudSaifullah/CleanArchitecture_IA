3cdf7c6e2307918e7790a88c1e6a3600d0414338
Mr. Anik Saha <anik.bd@asa-international.com>
2022-07-06 15:57:22 +0600

    IA-31 Pie chart and bar chart add

--- src/ui/internal-audit/src/app/views/private/dashboard/dashboard.component.ts
+++ src/ui/internal-audit/src/app/views/private/dashboard/dashboard.component.ts
@@ -11,5 +11,56 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  data = {
+    type: 'line',
+    labels: ['Rare', 'Unlikely', 'Moderate', 'Likeley', 'Almost Certain'],
+    datasets: [
+      {
+        label: 'Significant',
+        backgroundColor: '#36A2EB',
+        data: [40, 40 , 100 , 150, 230]
+      },
+      {
+        label: 'Extreme',
+        backgroundColor: '#ff0000',
+        data: [50, 10, 200, 130, 50]
+      },
+      {
+        label: 'High',
+        backgroundColor: '#d97373',
+        data: [150, 200, 50, 60, 90]
+      },
+      {
+        label: 'Moderate',
+        backgroundColor: '#067a23',
+        data: [160, 240, 150, 90, 70]
+      },
+      {
+        label: 'Low',
+        backgroundColor: '#05ed3f',
+        data: [200, 50, 200, 180, 100]
+      }
+    ],
+    options: {
+      plugins: {
+          title: {
+              display: true,
+              text: 'Custom Chart Title'
+          }
+      }
+  }
+  };
+  
+  chartPieData = {
+    labels: ['Reject & Close', 'Approved', 'Reject with Feedback','Send for Approval'],
+    datasets: [
+      {
+        data: [150, 50, 100, 200],
+        backgroundColor: ['#FF6384','#05ed3f', '#FFCE56', '#36A2EB'],
+        hoverBackgroundColor: ['#FF6384','#05ed3f', '#FFCE56','#36A2EB']
+      }
+    ]
+  };
 
 }

bc97dc131abbbe69326a89b890fde1d518c43a91
Ms.Ishita Sur Apan <Ishita.bd@asa-international.com>
2022-07-18 11:12:44 +0600

    Topic head migration

--- src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/InternalAuditContext.cs
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/InternalAuditContext.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using Internal.Audit.Domain.Entities.Common;
 using Internal.Audit.Domain.Entities.Config;
 using Internal.Audit.Domain.Entities.config;
+using Internal.Audit.Domain.Entities.BranchAudit;
 
 namespace Internal.Audit.Infrastructure.Persistent;
 

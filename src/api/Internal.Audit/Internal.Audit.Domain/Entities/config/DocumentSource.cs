ac45807202a6533cb5289db14714efa031dfe6e3
iftekharalibd <ali.bd@asa-international.com>
2022-07-19 09:50:16 +0600

    IA-86 document table added need migration

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/config/DocumentSource.cs
@@ -0,0 +1,18 @@
+﻿using Internal.Audit.Domain.Common;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Internal.Audit.Domain.Entities.config;
+
+[Table("Document", Schema = "config")]
+public class DocumentSource : EntityBase
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [DefaultValue("1")]
+    public bool IsActive { get; set; }
+}

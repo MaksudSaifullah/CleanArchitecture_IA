5d811bf2f4155bfe7d884315276ff4eacee73b01
iftekharalibd <ali.bd@asa-international.com>
2022-07-19 11:13:55 +0600

    Document source Added from Context seed

--- src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/InternalAuditContextSeed.cs
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/InternalAuditContextSeed.cs
@@ -1,5 +1,6 @@
 ﻿
 using Internal.Audit.Domain.Entities;
+using Internal.Audit.Domain.Entities.config;
 using Internal.Audit.Domain.Entities.Security;
 
 namespace Internal.Audit.Infrastructure.Persistent;
@@ -9,8 +10,13 @@ public class InternalAuditContextSeed
     public static async Task SeedAsync(InternalAuditContext context)
     {
         if (!context.Users.Any())
-        { 
-            context.Users.AddRange(GetSeedUsers());
+        {
+            context.Users.AddRange(GetSeedUsers());           
+            await context.SaveChangesAsync();
+        }
+        if (!context.DocumentSources.Any())
+        {
+            context.DocumentSources.AddRange(GetInitialDocumentSource());
             await context.SaveChangesAsync();
         }
     }
@@ -24,6 +30,33 @@ public class InternalAuditContextSeed
                 UserName = "Admin", Password = "@dmin123", IsPasswordExpired = false, IsEnabled = true, IsDeleted = false,
                 IsAccountExpired = false, IsAccountLocked = false
             }
+
+        };
+    }
+    private static IEnumerable<DocumentSource> GetInitialDocumentSource()
+    {
+        return new List<DocumentSource>
+        {
+            new DocumentSource
+            {
+               Name ="Profile_Picture",CreatedBy="system",CreatedOn=DateTime.Now
+            },
+            new DocumentSource
+            {
+               Name ="Approval_Evidence",CreatedBy="system",CreatedOn=DateTime.Now
+            },
+             new DocumentSource
+            {
+               Name ="Review_Evidence",CreatedBy="system",CreatedOn=DateTime.Now
+            },
+              new DocumentSource
+            {
+               Name ="Testing_Sheets",CreatedBy="system",CreatedOn=DateTime.Now
+            },
+               new DocumentSource
+            {
+               Name ="Evidence_Details",CreatedBy="system",CreatedOn=DateTime.Now
+            },
 
         };
     }

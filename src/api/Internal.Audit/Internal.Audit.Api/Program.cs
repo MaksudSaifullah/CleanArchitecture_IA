cb619196ef5853c21080dd3471e852c2ef3d51aa
iftekharalibd <ali.bd@asa-international.com>
2022-07-26 15:17:33 +0600

    MQ service in main program

--- src/api/Internal.Audit/Internal.Audit.Api/Program.cs
+++ src/api/Internal.Audit/Internal.Audit.Api/Program.cs
@@ -4,7 +4,7 @@ using Internal.Audit.Application;
 using Internal.Audit.Application.Common;
 using Internal.Audit.Infrastructure.Notification;
 using Internal.Audit.Infrastructure.Persistent;
-using Internal.Audit.MQ.Service;
+//using Internal.Audit.MQ.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
@@ -59,7 +59,7 @@ builder.Services.AddApiServices();
 builder.Services.AddApplicationServices();
 builder.Services.AddPersistentInfrastructureServices(builder.Configuration);
 builder.Services.AddInfrastructureNotificationsServices(builder.Configuration);
-builder.Services.AddInfrastructureMQServices(builder.Configuration);
+//builder.Services.AddInfrastructureMQServices(builder.Configuration);
 
 builder.Services.AddApiVersioning(config =>
 {

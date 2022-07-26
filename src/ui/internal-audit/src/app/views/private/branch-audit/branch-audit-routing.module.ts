036d6ea17d5d4a6aa67fbbdb618fca70f41de799
Ms. Ishrat Jahan Emu <emu.bd@asa-international.com>
2022-07-26 15:50:33 +0600

    IA-56

--- src/ui/internal-audit/src/app/views/private/branch-audit/branch-audit-routing.module.ts
+++ src/ui/internal-audit/src/app/views/private/branch-audit/branch-audit-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuditFrequencyComponent } from './audit-frequency/audit-frequency/audit-frequency.component';
+import { AuditFrequencyComponent } from './audit-frequency/audit-frequency.component';
 import { RiskAssessmentComponent } from './risk-assessment/risk-assessment.component';
 import { RiskCriteriaComponent } from './risk-criteria/risk-criteria.component';
 import { TopicHeadComponent } from './topic-head/topic-head.component';

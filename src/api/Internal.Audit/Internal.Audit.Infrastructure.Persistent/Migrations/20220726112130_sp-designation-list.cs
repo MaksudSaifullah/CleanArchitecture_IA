a4b92bf15dac55a508e9aadae708f75865549088
Ms.Ishita Sur Apan <Ishita.bd@asa-international.com>
2022-07-26 17:42:09 +0600

    store procedure migration

---
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/Migrations/20220726112130_sp-designation-list.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internal.Audit.Infrastructure.Persistent.Migrations
+{
+    public partial class spdesignationlist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER PROCEDURE [dbo].[GetDesignationListProcedure]
+	        @pageSize int,
+	        @pageNumber int,
+	        @searchTerm nvarchar(100)
+	 
+            AS
+            BEGIN
+	            SELECT [Id]
+	                ,[Name]
+                    ,[Description]
+                    ,[IsActive]
+                    ,[CreatedBy]
+                    ,[CreatedOn]
+	                FROM [common].[Designation]
+	                WHERE [IsDeleted] = 0
+	            AND ((@searchTerm IS NULL OR @searchTerm = '') OR (Designation.Name like '%'+@searchTerm+'%') OR (Designation.Description like '%'+@searchTerm+'%'))	
+
+	                ORDER BY [CreatedOn] DESC
+	                OFFSET ((@pageNumber-1) * @pageSize) ROWS
+	                FETCH NEXT @pageSize ROWS ONLY;
+
+
+	            SELECT cast(count(*) as bigint) as TotalCount from [common].[Designation] where [IsDeleted] = 0 
+	            AND ((@searchTerm IS NULL OR @searchTerm = '') OR (Designation.Name like '%'+@searchTerm+'%') OR (Designation.Description like '%'+@searchTerm+'%'))	
+
+            END
+            ");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE [dbo].[GetDesignationListProcedure]");
+        }
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/Migrations/20220726113901_sp-GetTopicHeadListProcedure.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internal.Audit.Infrastructure.Persistent.Migrations
+{
+    public partial class spGetTopicHeadListProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER   PROCEDURE [dbo].[GetTopicHeadListProcedure]
+                 @pageSize int,
+                 @pageNumber int,
+	             @searchTerm nvarchar(100)
+            AS
+            BEGIN
+                   SELECT
+	                 topicHead.[Id]
+		            ,topicHead.[CountryId]
+		            ,topicHead.[Name]
+		            ,topicHead.[EffectiveFrom]
+		            ,topicHead.[EffectiveTo]
+		            ,topicHead.[Description]
+                    ,Country.[Name] AS CountryName
+        	
+		            FROM [BranchAudit].[TopicHead] as topicHead
+	                INNER JOIN [common].Country as Country on Country.Id = topicHead.CountryId       
+		            WHERE topicHead.IsDeleted = 0
+		            AND ((@searchTerm IS NULL OR @searchTerm = '') OR (topicHead.[Name] like '%'+@searchTerm+'%') OR (Country.[Name] like '%'+@searchTerm+'%'))	
+
+                 ORDER BY topicHead.CreatedOn DESC
+                 OFFSET((@pageNumber - 1) * @pageSize) ROWS
+                 FETCH NEXT @pageSize ROWS ONLY;
+             SELECT cast(count(*) as bigint) as TotalCount from [BranchAudit].[TopicHead] as topicHead
+             INNER JOIN [common].Country as Country on Country.Id = topicHead.CountryId
+             where topicHead.[IsDeleted]=0 
+             AND ((@searchTerm IS NULL OR @searchTerm = '') OR (topicHead.[Name] like '%'+@searchTerm+'%') OR (Country.[Name] like '%'+@searchTerm+'%'))	
+            END
+
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE [dbo].[GetTopicHeadListProcedure]");
+        }
+    }
+}

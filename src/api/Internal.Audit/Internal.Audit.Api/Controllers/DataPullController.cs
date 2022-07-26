5854323979140ba956b3e7fbb30cb93671d8030f
iftekharalibd <ali.bd@asa-international.com>
2022-07-26 15:16:58 +0600

    Broker Interface added

---
+++ src/api/Internal.Audit/Internal.Audit.Api/Controllers/DataPullController.cs
@@ -0,0 +1,26 @@
+﻿using Internal.Audit.MQ.Service.Services;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Internal.Audit.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataPullController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public DataPullController(IMediator madiator)
+        {
+            _mediator = madiator ?? throw new ArgumentNullException(nameof(madiator));
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult> GetList()
+        {
+           
+            return Ok("");
+
+        }
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/DataPull/Commands/AddDataPullCommand/AddDataCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Internal.Audit.Application.Contracts.Persistent;
+using Internal.Audit.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Application.Features.DataPull.Commands.AddDataPullCommand;
+
+public class AddDataCommandHandler : IRequestHandler<AddDataCommand, AddDataPullCommandDTO>
+{
+    private readonly IUnitOfWork _unitOfWork;  
+    private readonly IMapper _mapper;    
+    private readonly IBrokerService _broker;
+    public AddDataCommandHandler( IMapper mapper, IBrokerService broker,
+       IUnitOfWork unitOfWork)
+    {
+       
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        _broker= broker ?? throw new ArgumentNullException(nameof(broker));
+    }
+    public Task<AddDataPullCommandDTO> Handle(AddDataCommand request, CancellationToken cancellationToken)
+    {
+        var message = new
+        {
+            ToDate = DateTime.Now.ToString("yyyy-MM-dd"),
+            FromDate = DateTime.Now.ToString("yyyy-MM-dd"),
+            CompareOnly = ""
+        };
+        _broker.Produce(message);
+        //var broker = new BrokerService(new Broker
+        //{
+        //    BrokerAddress = "amqp://guest:guest@localhost:5672",
+        //    BrokerQueue = "Directory_test"
+        //});
+        
+      //  return broker.Produce(message);
+        throw new NotImplementedException();
+    }
+}

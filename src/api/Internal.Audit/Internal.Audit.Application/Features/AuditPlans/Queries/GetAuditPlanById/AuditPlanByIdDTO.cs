d875ca421017840902d5e49bceac903895268b9b
Mr. Anik Saha <anik.bd@asa-international.com>
2022-07-31 17:55:43 +0600

    IA-131-RiskAssessmentBy Country

--- src/api/Internal.Audit/Internal.Audit.Application/Features/AuditPlans/Queries/GetAuditPlanById/AuditPlanByIdDTO.cs
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/AuditPlans/Queries/GetAuditPlanById/AuditPlanByIdDTO.cs
@@ -9,9 +9,9 @@ namespace Internal.Audit.Application.Features.AuditPlans.Queries.GetAuditPlanByI
     public record AuditPlanByIdDTO
     {
         public Guid Id { get; set; }
-        public string? CountryName { get; set; }
-        public string? AuditTypeName { get; set; }
-        public string? RiskAssessmentCode { get; set; }
+        public Guid CountryId { get; set; }
+        public Guid AuditTypeId { get; set; }
+        public string? AssessmentCode { get; set; }
         public Guid RiskAssessmentId { get; set; }
         public string? PlanCode { get; set; }
         public Guid PlanningYearId { get; set; }

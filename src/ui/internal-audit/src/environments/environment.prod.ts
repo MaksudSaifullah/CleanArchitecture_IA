91aaf181600d3ef67e620e44b3406a974cb4d346
iftekharalibd <ali.bd@asa-international.com>
2022-08-01 14:59:19 +0600

    Qa a port updated

--- src/ui/internal-audit/src/environments/environment.prod.ts
+++ src/ui/internal-audit/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true, 
-  hostName: 'http://192.168.97.56:9999/api/v1',
+  hostName: 'http://192.168.97.56:4000/api/v1',
   captcha_public_key:'6LdcoxohAAAAAOIxeTBNMubRhgEu_clJPnFRa0uP',
   upload_file_configuration :{
     "id": "24eb5c76-2107-ed11-b3b2-00155d610b18",
     "name": "Profile_Picture"
   },
   file_upload_url: 'Document',
-  file_host: 'http://192.168.97.56:9999'
+  file_host: 'http://192.168.97.56:4000'
 };

1fca7492093ca9c783b1f63ff7eed6a4a25bb1d1
iftekharalibd <ali.bd@asa-international.com>
2022-08-08 13:07:35 +0600

    Seed issue solved

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/BranchAudit/RiskAssesmentData.cs
@@ -0,0 +1,14 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.BranchAudit;
+
+public class RiskAssesmentData:EntityBase
+{
+
+
+}

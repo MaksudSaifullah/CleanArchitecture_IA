47a1b6d3c347c524af0a22438c0ee5e5759e117d
iftekharalibd <ali.bd@asa-international.com>
2022-08-17 17:01:18 +0600

    Document source relation removed

---
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/Migrations/20220817105758_Document Source Relation removed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internal.Audit.Infrastructure.Persistent.Migrations
+{
+    public partial class DocumentSourceRelationremoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Document_DocumentSource_DocumentSourceId",
+                schema: "Common",
+                table: "Document");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Document_DocumentSourceId",
+                schema: "Common",
+                table: "Document");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Document_DocumentSourceId",
+                schema: "Common",
+                table: "Document",
+                column: "DocumentSourceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Document_DocumentSource_DocumentSourceId",
+                schema: "Common",
+                table: "Document",
+                column: "DocumentSourceId",
+                principalSchema: "config",
+                principalTable: "DocumentSource",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

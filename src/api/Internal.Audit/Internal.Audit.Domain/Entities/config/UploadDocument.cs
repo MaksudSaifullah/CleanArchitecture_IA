058ef15389fc55160f43876e581193bec2c517b1
iftekharalibd <ali.bd@asa-international.com>
2022-08-17 18:10:38 +0600

    Uploaddocument page table

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/config/UploadDocument.cs
@@ -0,0 +1,23 @@
+﻿using Internal.Audit.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.config;
+
+[Table("UploadDocument", Schema = "Config")]
+public class UploadDocument:EntityBase
+{
+    public string DocumentVersion { get; set; }
+    public string Description { get; set; }
+    public string ApprovedBy { get; set; }
+    public DateTime ActiveFrom { get; set; }
+    public DateTime ActiveTo { get; set; }
+    public string UploadedBy { get; set; }
+    public virtual  ICollection<UploadedDocumentsNotify> UploadedDocumentsNotify { get; set; }
+
+
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/config/UploadedDocumentsNotify.cs
@@ -0,0 +1,21 @@
+﻿using Internal.Audit.Domain.Common;
+using Internal.Audit.Domain.Entities.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.config;
+
+[Table("UploadedDocumentsNotify", Schema = "Config")]
+public class UploadedDocumentsNotify:EntityBase
+{
+    public Guid RoleId{ get; set; }
+    [DefaultValue("0")]
+    public bool IsMailSent { get; set; }
+    [ForeignKey("RoleId")]
+    public virtual Role DocumentSource { get; set; } = null!;
+}

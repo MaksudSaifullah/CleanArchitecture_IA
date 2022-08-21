a458aa054624411555e14357a91b114fb4213ba8
iftekharalibd <ali.bd@asa-international.com>
2022-08-21 18:01:35 +0600

    Userprofile query need solve

--- src/api/Internal.Audit/Internal.Audit.Application/Features/UserRegistration/Queries/GetUserProfile/UserProfileQueryHandler.cs
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/UserRegistration/Queries/GetUserProfile/UserProfileQueryHandler.cs
@@ -18,14 +18,15 @@ namespace Internal.Audit.Application.Features.UserRegistration.Queries.GetUserPr
         public async Task<UserProfileQueryResponseDTO> Handle(UserProfileQuery request, CancellationToken cancellationToken)
         {
             var currentUserEmail = _currentAuthService.Email;
+          //  currentUserEmail = "sadsa";
             var user = await _userProfileRepository.GetByEmail(currentUserEmail);
             return new UserProfileQueryResponseDTO
             {
-                ProfileImageUrl = user == null? "": user.ProfileImageUrl,
-                FullName = user == null ? ""  : user.Employee == null ? "": user.Employee.Name,
-                DesignationName= user == null ? "" : user.Employee == null ? "" : user.Employee.Designation == null? "": user.Employee.Designation.Name,
-                Id = user.Id,
-                UserName=user.UserName,
+                ProfileImageUrl = user == null? "": user.ProfileImageUrl==null?"": user.ProfileImageUrl,
+                FullName = user == null ? ""  : user.Employee == null ? "": user.Employee.Name == null ? "": user.Employee.Name,
+                DesignationName= user == null ? "" : user.Employee == null ? "" : user.Employee.Designation == null? "": user.Employee.Designation.Name ==null? "": user.Employee.Designation.Name,
+                Id = user == null ? Guid.NewGuid():user.Id,
+                UserName= user == null ? "" : user.UserName,
             };
         }
     }

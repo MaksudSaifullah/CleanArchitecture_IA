6c080eae9ba69f499c9da172aadd6d994b6f407e
iftekharalibd <ali.bd@asa-international.com>
2022-08-23 13:45:13 +0600

    Audit Schedule By Id Dapper get done

--- src/api/Internal.Audit/Internal.Audit.Application/Mappings/MappingProfile.cs
+++ src/api/Internal.Audit/Internal.Audit.Application/Mappings/MappingProfile.cs
@@ -155,6 +155,7 @@ using Internal.Audit.Application.Features.CommonValueAndTypes.Queries.GetBranchb
 using Internal.Audit.Application.Features.AmbsDataSyncs.Queries.GetRiskAssesmentData;
 using Internal.Audit.Application.Features.UploadDocuments.Commands.AddUploadDocument;
 using Internal.Audit.Application.Features.UploadDocuments.Queries.GetUploadedDocumentListByRoled;
+using Internal.Audit.Application.Features.AuditSchedules.Queries.GetAuditScheduleByPlanId;
 
 namespace Internal.Audit.Application.Mappings;
 
@@ -424,7 +425,14 @@ public class MappingProfile : Profile
         CreateMap<UploadedDocumentsNotify, UploadedDocumentsNotifyCommand>().ReverseMap();
         CreateMap<UploadDocument, GetUploadedDocumentLstByRoleIdDTO>().ReverseMap();
         CreateMap<UploadedDocumentsNotify, UploadedDocumentsNotifyDTO>().ReverseMap();
-        CreateMap<Document, DocumentDTOc>().ReverseMap();
+        CreateMap<Document, DocumentDTOc>().ReverseMap();//
+        CreateMap<AuditCreationDTOs, AuditCreation>().ReverseMap();//
+        CreateMap<AuditScheduleParticipantsDTOs, AuditScheduleParticipants>().ReverseMap();//AuditScheduleBranchs
+        CreateMap<AuditScheduleBranchs, AuditScheduleBranch>().ReverseMap();//AuditTypeDTOs
+        CreateMap<AuditTypeDTOs, AuditType>().ReverseMap();//AuditTypeDTOs
+        CreateMap<UserDTOs, User>().ReverseMap();//EmployeeDTOs
+        CreateMap<EmployeeDTOs, Employee>().ReverseMap();//EmployeeDTOs
+        CreateMap<GetAuditSchedulePlanIdResponseDTO, AuditSchedule>().ReverseMap();//GetAuditSchedulePlanIdResponseDTO
 
 
 

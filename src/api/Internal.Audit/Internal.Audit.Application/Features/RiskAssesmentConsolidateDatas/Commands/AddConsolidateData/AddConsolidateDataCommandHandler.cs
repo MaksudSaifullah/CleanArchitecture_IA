88babbeefb49ea0fde70733e13a7b558aa5455c6
iftekharalibd <ali.bd@asa-international.com>
2022-08-25 13:13:23 +0600

    Risk assesment Avg data added

---
+++ src/api/Internal.Audit/Internal.Audit.Application/Features/RiskAssesmentConsolidateDatas/Commands/AddConsolidateData/AddConsolidateDataCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Internal.Audit.Application.Contracts.Persistent;
+using Internal.Audit.Application.Contracts.Persistent.RiskAssesmentConsolidateDatas;
+using Internal.Audit.Domain.Entities.BranchAudit;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Application.Features.RiskAssesmentConsolidateDatas.Commands.AddConsolidateData;
+
+public class AddConsolidateDataCommandHandler : IRequestHandler<AddConsolidateDataCommand, AddConsolidateDataResponseDTO>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IRiskAssesmentConsolidateDataCommandRepository _repository;   
+    private readonly IMapper _mapper;
+
+    public AddConsolidateDataCommandHandler(IRiskAssesmentConsolidateDataCommandRepository repository, IMapper mapper,
+        IUnitOfWork unitOfWork)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));      
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+    public async Task<AddConsolidateDataResponseDTO> Handle(AddConsolidateDataCommand request, CancellationToken cancellationToken)
+    {
+       
+        var newRiskdataConsolidate = await _repository.Add(_mapper.Map<RiskAssesmentConsolidateData>(request));
+        var rowsAffected = await _unitOfWork.CommitAsync();
+        return new AddConsolidateDataResponseDTO(newRiskdataConsolidate.Id, rowsAffected > 0, rowsAffected > 0 ? "RiskAssesment consolidate  created Successfully!" : "Error while creating iskAssesment consolidate!");
+
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/Repositories/RiskAssesmentConsolidateDatas/RiskAssesmentConsolidateDataQueryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Internal.Audit.Application.Contracts.Persistent.RiskAssesmentConsolidateDatas;
+using Internal.Audit.Domain.Entities.BranchAudit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Infrastructure.Persistent.Repositories.RiskAssesmentConsolidateDatas;
+
+public class RiskAssesmentConsolidateDataQueryRepository : QueryRepositoryBase<RiskAssesmentConsolidateData>, IRiskAssesmentConsolidateDataQueryrepository
+{
+    public RiskAssesmentConsolidateDataQueryRepository(string _connectionString) : base(_connectionString)
+    {
+    }
+
+    public async  Task<IEnumerable<RiskAssesmentConsolidateData>> GetAllAsync(Guid RiskAssesmentId,Guid CountryId)
+    {
+        var query = "EXEC [dbo].[BA_RiskAssesment_Consolidate] @countryid,@riskassesmentid";
+        var parameters = new Dictionary<string, object> { { "countryid", CountryId }, { "riskassesmentid", RiskAssesmentId } };
+        return await Get(query, parameters);
+    }
+}

fe68d0fa392e80796901ece67dfab0bf1ad1da60
Ms. Ishrat Jahan Emu <emu.bd@asa-international.com>
2022-08-29 16:48:17 +0600

    349

---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/BranchAudit/NotificationToAuditee.cs
@@ -0,0 +1,28 @@
+﻿using Internal.Audit.Domain.Common;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Internal.Audit.Domain.Entities.BranchAudit;
+[Table("NotificationToAuditee", Schema = "BranchAudit")]
+public class NotificationToAuditee : EntityBase
+{
+
+    [Required]
+    public Guid AuditCreationId { get; set; }
+
+    [Required]
+    [DefaultValue("0")]
+    public bool IsSent { get; set; }
+
+
+    [ForeignKey("AuditCreationId")]
+    public virtual AuditCreation AuditCreation { get; set; } = null!;
+
+    //public virtual ICollection<UserCountry> RiskOwnerTo { get; set; } = null!;
+    //public virtual ICollection<OthersTo> OthersTo { get; set; } = null!;
+
+    public virtual ICollection<AuditScheduleParticipants> AuditScheduleParticipants { get; set; } = null!;
+    public virtual ICollection<AuditScheduleBranch> AuditScheduleBranch { get; set; } = null!;
+
+}

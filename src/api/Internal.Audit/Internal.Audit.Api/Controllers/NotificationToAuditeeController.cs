b530eafb1a1a6838ca7673fef07ecaae68b95aee
Ms. Ishrat Jahan Emu <emu.bd@asa-international.com>
2022-09-04 09:22:37 +0600

    IA-349

---
+++ src/api/Internal.Audit/Internal.Audit.Api/Controllers/NotificationToAuditeeController.cs
@@ -0,0 +1,51 @@
+﻿using Internal.Audit.Application.Features.ClosingMeetingMinutes.Commands.AddClosingMeetingMinute;
+using Internal.Audit.Application.Features.ClosingMeetingMinutes.Commands.DeleteClosingMeetingMinute;
+using Internal.Audit.Application.Features.ClosingMeetingMinutes.Commands.UpdateClosingMeetingMinute;
+using Internal.Audit.Application.Features.ClosingMeetingMinutes.Queries.GetClosingMeetingMinuteList;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Internal.Audit.Api.Controllers
+{
+    [Route("api/v{version:apiVersion}/notificationToAuditee")]
+    [ApiController]
+    public class NotificationToAuditeeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public NotificationToAuditeeController(IMediator madiator)
+        {
+            _mediator = madiator ?? throw new ArgumentNullException(nameof(madiator));
+        }
+
+        [HttpPost("paginated")]
+        public async Task<ActionResult<ClosingMeetingMinuteListPagingDTO>> GetList(GetClosingMeetingMinuteListQuery getClosingMeetingMinuteListQuery)
+        {
+            var closingMeetingMinutes = await _mediator.Send(getClosingMeetingMinuteListQuery);
+            return Ok(closingMeetingMinutes);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AddClosingMeetingMinuteResponseDTO>> Add(AddClosingMeetingMinuteCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<UpdateClosingMeetingMinuteResponseDTO>> Update(UpdateClosingMeetingMinuteCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult<DeleteClosingMeetingMinuteResponseDTO>> Delete(Guid Id)
+        {
+            var command = new DeleteClosingMeetingMinuteCommand(Id);
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ src/api/Internal.Audit/Internal.Audit.Domain/Entities/BranchAudit/NotifedUsersTo.cs
@@ -0,0 +1,22 @@
+﻿using Internal.Audit.Domain.Common;
+using Internal.Audit.Domain.Entities.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Audit.Domain.Entities.BranchAudit;
+
+
+[Table("NotifedUsersTo", Schema = "BranchAudit")]
+public class NotifedUsersTo : EntityBase
+{
+
+    public Guid UserId { get; set; }
+    public Guid NotificationToAuditeesId { get; set; }
+
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; } = null!;
+}

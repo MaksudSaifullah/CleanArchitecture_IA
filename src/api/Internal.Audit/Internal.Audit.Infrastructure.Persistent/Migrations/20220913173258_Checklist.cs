69c65dc37bf7b638353b50f1af5c8a56b50f00f5
Mr. Anik Saha <anik.bd@asa-international.com>
2022-09-13 23:35:19 +0600

    IA-351-CheckList-MIgration

---
+++ src/api/Internal.Audit/Internal.Audit.Infrastructure.Persistent/Migrations/20220913173258_Checklist.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Internal.Audit.Infrastructure.Persistent.Migrations
+{
+    public partial class Checklist : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Checklists",
+                schema: "BranchAudit",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "newsequentialid()"),
+                    ChecklistCode = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AuditScheduleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AuditScheduleBranchId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OpeningDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DisbursementDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BranchManagerName = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    BMJoiningDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuditDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuditOn = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    LastAuditFromDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastAuditToDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDraft = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "0"),
+                    CreatedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ReviewedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ReviewedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ApprovedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ApprovedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "0")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checklists", x => x.Id);
+                });
+
+           
+
+            migrationBuilder.CreateTable(
+                name: "ChecklistTopics",
+                schema: "BranchAudit",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "newsequentialid()"),
+                    TopicHeadId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ChecklistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ReviewedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ReviewedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ApprovedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ApprovedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "0")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChecklistTopics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChecklistTopics_Checklists_ChecklistId",
+                        column: x => x.ChecklistId,
+                        principalSchema: "BranchAudit",
+                        principalTable: "Checklists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChecklistTopics_TopicHead_TopicHeadId",
+                        column: x => x.TopicHeadId,
+                        principalSchema: "BranchAudit",
+                        principalTable: "TopicHead",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+           
+
+           
+            migrationBuilder.CreateTable(
+                name: "ChecklistTopicDetails",
+                schema: "BranchAudit",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "newsequentialid()"),
+                    ChecklistTopicId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    QuestionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TestStepId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DocumentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TestingResult = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TestingConclusion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalScore = table.Column<int>(type: "int", nullable: false),
+                    ObtainedScore = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false),
+                    WeightedScore = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ReviewedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ReviewedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ApprovedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ApprovedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "0")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChecklistTopicDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChecklistTopicDetails_ChecklistTopics_ChecklistTopicId",
+                        column: x => x.ChecklistTopicId,
+                        principalSchema: "BranchAudit",
+                        principalTable: "ChecklistTopics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChecklistTopicDetails_ChecklistTopicId",
+                schema: "BranchAudit",
+                table: "ChecklistTopicDetails",
+                column: "ChecklistTopicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChecklistTopics_ChecklistId",
+                schema: "BranchAudit",
+                table: "ChecklistTopics",
+                column: "ChecklistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChecklistTopics_TopicHeadId",
+                schema: "BranchAudit",
+                table: "ChecklistTopics",
+                column: "TopicHeadId");
+
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChecklistTopicDetails",
+                schema: "BranchAudit");
+
+           
+
+            migrationBuilder.DropTable(
+                name: "ChecklistTopics",
+                schema: "BranchAudit");
+
+
+            migrationBuilder.DropTable(
+                name: "Checklists",
+                schema: "BranchAudit");
+        }
+    }
+}
